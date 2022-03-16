dec1e9871dc3b32736e192b64d46b3b56c31dde8
JuanMigone <juan.migone@balloon-group.com>
2022-03-16 00:46:47 -0300

    prueba para arreglar onclick mobile

--- components/CryptoPrices.vue
+++ components/CryptoPrices.vue
@@ -2,31 +2,49 @@
 <template>
   <div class="app">
     <div class="primeraSeccion">
-    <div class="titulo">Precios de Cryptomonedas</div> 
-    <select class="selector" name="MonedaSeleccionada" id="" >
-      <option  value="" v-for="(item,index) in ListaMonedas" :key="item+index" @click="setearMoneda(item)">
-        {{item}}
-      </option>
-    </select>   
+      <div class="titulo">Precios de Cryptomonedas</div>
+      <select class="selector" name="MonedaSeleccionada2" id="" @change="onChange($event)">
+        <!-- @click="setearMoneda(item)" -->
+        <option
+          :value="item"
+          v-for="(item, index) in ListaMonedas"
+          :key="item + index"
+          
+        >
+          {{ item }}
+        </option>
+      </select>
     </div>
     <div class="infoMoneda">
-    <div>{{this.objMonedaSeleccionada.id?this.objMonedaSeleccionada.id.toUpperCase():"BITCOIN"}}</div>
-    <img :src="objMonedaSeleccionada.image.large" alt="">
-    <div class="precios">
-    <div class="preciotitulo">Price:</div>
-    <div class="preciovalor">{{(this.objMonedaSeleccionada.market_data.current_price.usd).toLocaleString('en-US', {
-  style: 'currency',
-  currency: 'USD',
-})}}</div>
+      <div>
+        {{
+          this.objMonedaSeleccionada.id
+            ? this.objMonedaSeleccionada.id.toUpperCase()
+            : "BITCOIN"
+        }}
+      </div>
+      <img :src="objMonedaSeleccionada.image.large" alt="" />
+      <div class="precios">
+        <div class="preciotitulo">Price:</div>
+        <div class="preciovalor">
+          {{
+            this.objMonedaSeleccionada.market_data.current_price.usd.toLocaleString(
+              "en-US",
+              {
+                style: "currency",
+                currency: "USD",
+              }
+            )
+          }}
+        </div>
+      </div>
+      <div class="descripcion">
+        <div class="desctitulo">Description</div>
+        <div class="descvalor">
+          {{ this.objMonedaSeleccionada.description.en }}
+        </div>
+      </div>
     </div>
-    <div class="descripcion">
-    <div class="desctitulo">Description</div>
-    <div class="descvalor">{{this.objMonedaSeleccionada.description.en}}</div>
-    </div>
-    </div>
-   
-    
-    
   </div>
 </template>
 
@@ -41,57 +59,62 @@ export default Vue.extend({
       ListaMonedas: [],
       MonedaSeleccionada: "bitcoin",
       objMonedaSeleccionada: {
-        image: { large: null},
-        description: { en: null},
-        market_data: {current_price: {usd: 0}}
-        },
+        image: { large: null },
+        description: { en: null },
+        market_data: { current_price: { usd: 0 } },
+      },
     };
   },
 
   async mounted() {
-   
-         axios
-         .get("https://api.coingecko.com/api/v3/coins/bitcoin")
-         .then((resp) => {
-           this.objMonedaSeleccionada = resp.data;
-          //  console.log(this.precios);
-         });
-          axios
-         .get("https://api.coingecko.com/api/v3/coins")
-         .then((resp) => {
-           this.ListaMonedas = resp.data.map(moneda => moneda.id);
-           console.log(this.ListaMonedas);
-         });
-
-         
+    axios.get("https://api.coingecko.com/api/v3/coins/bitcoin").then((resp) => {
+      this.objMonedaSeleccionada = resp.data;
+      //  console.log(this.precios);
+    });
+    axios.get("https://api.coingecko.com/api/v3/coins").then((resp) => {
+      this.ListaMonedas = resp.data.map((moneda) => moneda.id);
+      console.log(this.ListaMonedas);
+    });
   },
-  methods: {
-    setearMoneda(item) {
-      this.MonedaSeleccionada = item; console.log(this.MonedaSeleccionada)
-    }
-  },
-  watch: {
-    MonedaSeleccionada: function(newVal) {
+   methods: {
+  //   setearMoneda(item) {
+      
+  //     this.MonedaSeleccionada = item;
+  //     // console.log(this.MonedaSeleccionada);
+  //   },
+    onChange(event) {
       axios
-         .get("https://api.coingecko.com/api/v3/coins/"+newVal)
-         .then((resp) => {
-           this.objMonedaSeleccionada = resp.data;
+        .get("https://api.coingecko.com/api/v3/coins/" + event.target.value)
+        .then((resp) => {
+          this.objMonedaSeleccionada = resp.data;
           console.log(this.objMonedaSeleccionada);
-         });
-    }
-  }
+        });
+            console.log(event.target.value)
+        }
+  },
+  //  watch: {
+  //    MonedaSeleccionada2: function (newVal) {
+  //      axios
+  //        .get("https://api.coingecko.com/api/v3/coins/" + newVal)
+  //        .then((resp) => {
+  //          this.objMonedaSeleccionada = resp.data;
+  //          console.log(this.objMonedaSeleccionada);
+  //        });
+  //    },
+    
+  //  },
 });
 </script>
 
 <style scoped>
 .app {
-  margin: 0px 200px
+  margin: 0px 200px;
 }
 .primeraSeccion {
   width: 100%;
   padding: auto;
   display: flex;
-  flex-direction: row;  
+  flex-direction: row;
   margin-top: 40px;
   margin-bottom: 40px;
 }
@@ -104,7 +127,7 @@ export default Vue.extend({
   color: green;
 }
 .selector {
-  margin: auto;   
+  margin: auto;
   text-align: center;
   background-color: rgb(222, 255, 155);
   font-size: 30px;
@@ -137,7 +160,7 @@ export default Vue.extend({
   text-align: left;
   background-color: rgb(204, 241, 204);
   border-radius: 35px;
-  padding: 20px
+  padding: 20px;
 }
 .desctitulo {
   margin-bottom: 25px;
@@ -148,39 +171,31 @@ export default Vue.extend({
 
 @media (max-width: 1024px) {
   .app {
-  margin: 0px;
-}
-.descripcion {
-  border-radius: 0px;
-  text-align: center;
-}
-
+    margin: 0px;
+  }
+  .descripcion {
+    border-radius: 0px;
+    text-align: center;
+  }
 }
 
 @media (max-width: 1280px) {
-
-
-.primeraSeccion {
-  flex-direction: column;  
-}
-.titulo {
-  margin-bottom: 30px;
-  width: auto;
-  font-size: 45px;
-  font-style: bolder;
-  text-align: center;
-  color: green;
-}
-
+  .primeraSeccion {
+    flex-direction: column;
+  }
+  .titulo {
+    margin-bottom: 30px;
+    width: auto;
+    font-size: 45px;
+    font-style: bolder;
+    text-align: center;
+    color: green;
+  }
 }
 
 @media (min-width: 1024px) {
-
-.app {
-  margin: 0px 10.8vw;
+  .app {
+    margin: 0px 10.8vw;
+  }
 }
-} 
-
-
-
 </style>
